207ae08150ebd28321f1be3490392c49ec280179
Franciscot93 <franciscot.p.93@gmail.com>
2023-07-01 22:10:19 -0300

    creacion del formulario, inputs y estilos de estos

--- Administrador_de_pacientes_react/src/components/Formulario.jsx
+++ Administrador_de_pacientes_react/src/components/Formulario.jsx
@@ -1,12 +1,90 @@
-import React from 'react'
+import React from "react";
 
 const Formulario = () => {
   return (
-    <div>
-      <h2>Formulario</h2>
+    <div className="md:w-1/2 lg:w-2/5">
+      <h2 className="font-black text-3xl text-center">Seguimiento Pacientes</h2>
+
+      <p className="text-lg mt-5 text-center mb-10">
+        Añade Pacientes y {""}
+        <span className="text-indigo-600 font-bold">Administralos</span>
+      </p>
+
+      <form className="bg-white shadow-md mb-10 rounded-lg py-10 px-5" action="">
+        <div className="mb-5">
+          <label
+            className="block text-gray-700 uppercase font-bold"
+            htmlFor="mascotaNombre"
+          >
+            Nombre Mascota
+          </label>
+          <input
+            id="mascotaNombre"
+            type="text"
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+            placeholder="Nombre de la mascota"
+          />
+        </div>
+        <div className="mb-5">
+          <label
+            className="block text-gray-700 uppercase font-bold"
+            htmlFor="propietarioNombre"
+          >
+            Nombre Propietario
+          </label>
+          <input
+            id="propietarioNombre"
+            type="text"
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+            placeholder="Nombre del propietario"
+          />
+        </div>
+        <div className="mb-5">
+          <label
+            className="block text-gray-700 uppercase font-bold"
+            htmlFor="emailPropietario"
+          >
+            E-MAIL
+          </label>
+          <input
+            id="emailPropietario"
+            type="email"
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+            placeholder="propietario@email.com"
+          />
+        </div>
+        <div className="mb-5">
+          <label
+            className="block text-gray-700 uppercase font-bold"
+            htmlFor="fechaDeAlta"
+          >
+            Fecha de alta
+          </label>
+          <input
+            id="fechaDeAlta"
+            type="date"
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+          />
+        </div>
+        <div className="mb-5">
+          <label
+            className="block text-gray-700 uppercase font-bold"
+            htmlFor="sintomas"
+          >
+            Sintomas
+          </label>
+          <textarea
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+            id="sintomas"
+            placeholder="Describe los sintomas"
+          />
+        </div>
+        <input type="submit" 
+        className="bg-indigo-600 w-full p-3 text-white hover:bg-indigo-700 cursor-pointer transition-colors uppercase font-bold"
+        value="Agregar Paciente"/>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Formulario
-
+export default Formulario;
